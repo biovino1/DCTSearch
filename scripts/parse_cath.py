d6c1921da29e1ff1c49891abdb4b8919145505a8
ben <biovino@gmail.com>
2024-01-08 15:53:57 -0500

    write cath20 seqs with hom IDs

--- scripts/parse_cath.py
+++ scripts/parse_cath.py
@@ -23,6 +23,46 @@ def download_file(url: str, filename: str):
     urllib.request.urlretrieve(url, f'data/{filename}')
 
 
+def parse_class(filename: str) -> dict:
+    """Returns a dictionary of classifications for each protein.
+
+    Args:
+        filename (str): Name of CATH classification file to parse.
+
+    Returns:
+        dict: dictionary where key is protein ID and value is homologous family ID
+    """
+
+    classes = {}
+    with open(filename, 'r', encoding='utf8') as file:
+        for line in file:
+            line = line.split()
+            classes[line[0]] = line[4]
+
+    return classes
+
+
+def write_classes(filename: str, classes: dict):
+    """Writes classifications for each protein to fasta file.
+
+    Args:
+        filename (str): Name of file to add classifications to.
+        classes (dict): Dictionary of classifications for each protein.
+    """
+
+    # Get all seqs from fasta file
+    seqs = {}
+    for seq in SeqIO.parse(filename, 'fasta'):
+        pid = seq.id.split('|')[2]
+        pid += f"\t{classes[pid.split('/')[0]]}"
+        seqs[pid] = str(seq.seq)
+
+    # Write to new fasta file
+    with open('data/cath20.fa', 'w', encoding='utf8') as file:
+        for pid, seq in seqs.items():
+            file.write(f'>{pid}\n{seq}\n')
+
+
 def main():
     """Main function
     """
@@ -31,9 +71,13 @@ def main():
     url1 = 'ftp://orengoftp.biochem.ucl.ac.uk/cath/releases/latest-release/' \
         'non-redundant-data-sets/cath-dataset-nonredundant-S20.fa'
     url2 = 'ftp://orengoftp.biochem.ucl.ac.uk/cath/releases/latest-release/' \
-        'cath-classification-data/cath-domain-list-S35.txt'
+        'cath-classification-data/cath-domain-list-S100.txt'
     download_file(url1, 'cath20.fa')
     download_file(url2, 'cath_class.txt')
+
+    # Parse classification file and add ID's to fasta file
+    classes = parse_class('data/cath_class.txt')
+    write_classes('data/cath20.fa', classes)
 
 
 if __name__ == '__main__':
