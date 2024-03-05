ce82a9a6debcf7364fe7b8cd3b75cbadff695907
ben <biovino@gmail.com>
2024-03-05 15:35:51 -0500

    batching as separate class

--- scripts/embedding.py
+++ scripts/embedding.py
@@ -6,7 +6,6 @@ __date__ = "2/19/24"
 """
 
 from dataclasses import dataclass, field
-import os
 import esm
 import torch
 import numpy as np
@@ -30,14 +29,17 @@ class Model:
 
     def load_esm2(self, checkpoint: str):
         """Loads ESM-2 model.
+
+        Args:
+            checkpoint (str): Model checkpoint to load.
         """
 
         if checkpoint == 't33':
-            self.encoder, alphabet = esm.pretrained.esm2_t33_650M_UR50D()
+            self.encoder, self.alphabet = esm.pretrained.esm2_t33_650M_UR50D()
         if checkpoint == 't30':
-            self.encoder, alphabet = esm.pretrained.esm2_t30_150M_UR50D()
+            self.encoder, self.alphabet = esm.pretrained.esm2_t30_150M_UR50D()
 
-        self.tokenizer = alphabet.get_batch_converter()
+        self.tokenizer = self.alphabet.get_batch_converter()
         self.encoder.eval()
 
 
@@ -53,109 +55,82 @@ class Model:
 
 @dataclass
 class Embedding:
-    """This class creates and stores the necessary information for protein sequences to be
-    embedded. An Embedding object is meant to be used in the Fingerprint class for quantization.
-    Embeddings can also be saved and loaded from file (npz).
-
-    Any number of sequences can be embedded at once to maximize GPU usage, however, make sure
-    you do not overload the GPU memory with too many sequences.
+    """This class stores the necessary information for a protein sequence to be embedded.
+    An Embedding is meant to be used in the Fingerprint class for quantization. Embeddings
+    can also be saved and loaded from file (npz).
 
     Attributes:
-        pid (list): Protein IDs.
-        seq (list): Protein sequences.
-        embed (list): Dictionaries of embeddings from each layer.
-        contacts (list): Numpy arrays of contact maps.
-        model (Model): Model class with encoder and tokenizer.
-        device (str): gpu/cpu
-        layers (list): List of layers to extract embeddings from.
+        pid (str): Protein ID.
+        seq (str): Protein sequence.
+        embed (dict): Dictionary of embeddings from each layer.
+        contacts (np.array): Contact map.
     """
-    pid: list = field(default_factory=list)
-    seq: list = field(default_factory=list)
-    embed: list = field(default_factory=list)
-    contacts: list = field(default_factory=list)
-    model: Model = field(default=None)
-    device: str = field(default='cpu')
-    layers: list = field(default_factory=list)
+    pid: str = field(default_factory=str)
+    seq: str = field(default_factory=str)
+    embed: dict = field(default_factory=dict)
+    contacts: np.array = field(default_factory=list)
 
 
-    def split_seq(self, maxlen: int, olp: int):
-        """Splits a sequence into smaller sequences of length maxlen with overlap and embeds
-        each subsequence. Overlapping regions in the embeddings and contact maps are averaged.
+    def __post_init__(self):
+        """Initialize contacts to array.
+        """
+
+        self.contacts = np.array([])
+
+
+    def split_seq(self, maxlen: int, overlap: int) -> list:
+        """Splits a sequence into smaller sequences of length maxlen with overlap.
 
         Args:
             maxlen (int): Maximum length of each sub-sequence
-            olp (int): Overlap between sub-sequences
+            overlap (int): Overlap between sub-sequences
+
+        Returns:
+            list: List of sub-sequences
         """
 
         subseqs = []
-        for i in range(0, len(self.seq[0]), maxlen-olp):  # only one seq in list
-            subseq = self.seq[0][i:i+maxlen]
-            if len(subseq) > olp:  # skip if subseq is too short to be unique
+        for i in range(0, len(self.seq), maxlen-overlap):
+            subseq = self.seq[i:i+maxlen]
+            if len(subseq) > overlap:  # skip if subseq is too short to be unique
                 subseqs.append(subseq)
 
-         # Extract embeddings and contact maps for each subsequence
-        edata = {}
-        for i, seq in enumerate(subseqs):
-            embs = [(self.pid, seq)]
-            embs = self.extract_embeds(embs)
-            if not edata:  # dynamic initialization in case different layers are used
-                edata = embs[0]  # only one seq in dictionary
-                continue
-
-            # Average overlapping positions between previous and current embeddings
-            for lay, emb in embs[0].items():
-                if lay == 'ct':
-                    edata[lay] = self.combine_contacts(edata[lay], emb, maxlen-olp, i)
-                    continue
-                edata[lay][-olp:] = (edata[lay][-olp:] + emb[:olp]) / 2
-                edata[lay] = np.concatenate((edata[lay], emb[olp:]), axis=0)
-
-        # Store embeddings and contact maps
-        self.embed = [{k: v for k, v in edata.items() if k in self.layers}]
-        self.contacts = [edata['ct']]
+        return subseqs
 
 
-    def extract_embeds(self, embs: list) -> dict:
-        """Returns a dictionary containing embeddings for every sequence in the object. Each
-        sequence will contain a dictionary of embeddings from each layer and the contact map.
+    def extract_embeds(self, seq: str, model: Model, device: str, layers: list) -> dict:
+        """Returns a dictionary containing embeddings from each layer and the contact map.
 
         Args:
-            embs (list): List of tuples of (protein ID, sequence) to embed.
-                ex. [(pid1, seq1), (pid2, seq2), ...]
+            seq (str): Protein sequence.
+            model (Model): Model class with encoder and tokenizer.
+            device (str): gpu/cpu
+            layer (list: List of layers to extract embeddings from.
 
         Returns:
-            dict: Nested dictionary of embeddings and contact maps.
-                ex. {0: {'ct': np.array, 'layer1': np.array, 'layer2': np.array, ...},
-                        1: {'ct': np.array, 'layer1': np.array, 'layer2': np.array, ...},
-                        ...}
+            dict: Dictionary containing embeddings from each layer and the contact map.
         """
 
-        # Tokenize batch and count length of each sequence
-        _, _, batch_tokens = self.model.tokenizer(embs)
-        batch_tokens = batch_tokens.to(self.device)
-        lengths = []
-        for batch in batch_tokens:
-            lengths.append(batch.ne(1).sum()-2)  # don't include padding, <bos> and <eos> tokens
+        _, _, batch_tokens = model.tokenizer([(self.pid, seq)])
+        batch_tokens = batch_tokens.to(device)
         try:
             with torch.no_grad():
-                results = self.model.encoder(batch_tokens,
-                                              repr_layers=self.layers, return_contacts=True)
+                results = model.encoder(batch_tokens, repr_layers=layers, return_contacts=True)
         except RuntimeError:
-            print(f'Error embedding {self.pid}')
+            print(f'Error embedding {self.pid}, length {len(self.seq)}')
             return {}
 
-        # Store results for each sequence
+        # Store results from each layer
         embs = {}
-        for i in (range(len(self.seq))):  #pylint: disable=C0200
-            embs[i] = {'ct': results["contacts"].cpu().numpy()[i][:lengths[i], :lengths[i]]}
-        for layer in self.layers:
-            for scount, emb in enumerate(results["representations"][layer].cpu().numpy()):
-                embs[scount][layer] = emb[1:lengths[scount]+1] # remove <bos> token
+        for layer in layers:
+            emb = results["representations"][layer]
+            embs[layer] = emb[0][1:-1] # remove <cls> and <eos>
+        embs['ct'] = results["contacts"][0]
 
         return embs
 
 
-    def combine_contacts(self, mat1: np.ndarray, mat2: np.ndarray, inc: int, times: int) -> np.ndarray:
+    def combine_contacts(self, mat1: torch.Tensor, mat2: torch.Tensor, inc: int, times: int) -> torch.Tensor:
         """Returns a larger square matrix combining two smaller square matrices.
 
         mat1 has values starting from the top left corner, mat2 has values starting from the bottom
@@ -163,13 +138,13 @@ class Embedding:
         determine by the inc argument.
 
         Args:
-            mat1 (numpy array): Running matrix of contacts (n x n).
-            mat2 (numpy array): Matrix to be added to mat1 (m x m).
+            mat1 (torch.Tensor): Running matrix of contacts (n x n).
+            mat2 (torch.Tensor): Matrix to be added to mat1 (m x m).
             inc (int): Number of indices to increase the size of the matrix by (inc x inc).
             times (int): Running total of times the function has been called.
 
         Returns:
-            numpy array: Matrix of combined contacts (n+inc x n+inc).
+            torch.Tensor: Matrix of combined contacts (n+inc x n+inc).
         """
 
         # Create new matrices to store combined contacts
@@ -199,38 +174,51 @@ class Embedding:
         return zeros1
 
 
-    def embed_seq(self, maxlen: int):
+    def embed_seq(self, model: Model, device: str, layers: list, maxlen: int):
         """Returns ESM-2 embedding and contact map of a protein sequence.
 
         Args:
-            layers (list: List of layers to extract embeddings from.
+            model (Model): Model class with encoder and tokenizer.
+            device (str): gpu/cpu
+            layer (list: List of layers to extract embeddings from.
             maxlen (int): Maximum length of sequence to embed
         """
 
-        if len(self.seq) == 1 and len(self.seq[0]) > maxlen:
-            olp = 200  # overlap between sub-sequences
-            self.split_seq(maxlen, olp)
-        else:
-            embs = list(zip(self.pid, self.seq))
-            embs = self.extract_embeds(embs)
-            for _, emb in embs.items():
-                self.embed.append({k: v for k, v in emb.items() if k in self.layers})
-                self.contacts.append(emb['ct'])
+        olp = 200  # overlap between sub-sequences
+        if len(self.seq) > maxlen:
+            subseqs = self.split_seq(maxlen, olp)
+        else:  # still need to make it a list for the loop
+            subseqs = [self.seq]
+
+        # Extract embeddings and contact maps for each subsequence
+        edata = {}
+        for i, seq in enumerate(subseqs):
+            embs = self.extract_embeds(seq, model, device, layers)
+            if not edata:  # dynamic initialization in case different layers are used
+                edata = embs
+                continue
+
+            # Average overlapping positions between previous and current embeddings
+            for lay, emb in embs.items():
+                if lay == 'ct':
+                    edata[lay] = self.combine_contacts(edata[lay], emb, maxlen-olp, i)
+                    continue
+                edata[lay][-olp:] = (edata[lay][-olp:] + emb[:olp]) / 2
+                edata[lay] = torch.cat((edata[lay], emb[olp:]), axis=0)
+
+        self.embed = {k: v.cpu().numpy() for k, v in edata.items() if k in layers}
+        self.contacts = edata['ct'].cpu().numpy()
 
 
-    def save(self, direc: str):
+    def save(self, filename: str):
         """Saves Embedding object to npz file.
 
         Args:
-            direc (str): Directory to save embeddings to.
+            filename (str): File to save embeddings to.
         """
 
-        if not os.path.exists(direc):
-            os.makedirs(direc)
-        for i, pid in enumerate(self.pid):
-            filename = f'{direc}/{pid}.npz'
-            np.savez_compressed(filename, pid=pid, seq=self.seq[i],
-                                embeds=self.embed[i], contacts=self.contacts[i])
+        np.savez_compressed(filename, pid=self.pid, seq=self.seq,
+                             embeds=self.embed, contacts=self.contacts)
 
 
     def load(self, filename: str):
@@ -241,8 +229,75 @@ class Embedding:
         """
 
         data = np.load(filename, allow_pickle=True)
-        self.pid.append(data['pid'])
-        self.seq.append(data['seq'])
-        self.embed.append(data['embeds'].item())
-        self.contacts.append(data['contacts'])
+        self.pid = data['pid']
+        self.seq = data['seq']
+        self.embed = data['embeds']
+        self.contacts = data['contacts']
         data.close()
+
+
+@dataclass
+class Batch:
+    """This class embeds a batch of protein sequences. Sequences can be embedded one at a time, but
+    parallel embedding of multiple sequences can be faster.
+
+    Attributes:
+        seqs (list): List of tuples containing (protein id, sequence)
+        model (Model): Model object containing encoder, alphabet, and tokenizer
+        device (str): gpu/cpu
+    """
+    seqs: list = field(default_factory=list)
+    model: Model = field(default_factory=Model)
+    device: str = field(default_factory=str)
+    embeds: list = field(default_factory=list)
+
+
+    def embed_batch(self, layers: list, maxlen: int):
+        """Embeds a batch of protein sequences.
+
+        Args:
+            layers (list): List of layers to extract embeddings from.
+            maxlen (int): Maximum length of sequence to embed.
+        """
+
+        if len(self.seqs) == 1:
+            self.embed_single(layers, maxlen)
+        else:
+            self.embed_parallel(layers)
+
+
+    def embed_single(self, layers: list, maxlen: int):
+        """Embeds a single protein sequence.
+
+        Args:
+            layers (list): List of layers to extract embeddings from.
+            maxlen (int): Maximum length of sequence to embed.
+        """
+
+        for seq in self.seqs:
+            emb = Embedding(pid=seq[0], seq=seq[1])
+            emb.embed_seq(self.model, self.device, layers, maxlen)
+            self.embeds.append(emb)
+
+
+    def embed_parallel(self, layers: list):
+        """Embeds a batch of protein sequences in parallel.
+
+        Args:
+            layers (list): List of layers to extract embeddings from.
+        """
+
+        _, _, batch_tokens = self.model.tokenizer(self.seqs)
+        batch_lens = (batch_tokens != self.model.alphabet.padding_idx).sum(1)
+        batch_tokens = batch_tokens.to(self.device)
+
+        # Embed sequences and parse results into individual Embedding objects
+        with torch.no_grad():
+            results = self.model.encoder(batch_tokens, repr_layers=layers, return_contacts=True)
+        for i, seq in enumerate(self.seqs):
+            emb = Embedding(pid=seq[0], seq=seq[1])
+            for layer, embed in results["representations"].items():
+                emb.embed[layer] = embed[i][1:batch_lens[i]-1].cpu().numpy()
+            emb.contacts = results["contacts"][i][:batch_lens[i]-2, :batch_lens[i]-2].cpu().numpy()
+            self.embeds.append(emb)
+        
