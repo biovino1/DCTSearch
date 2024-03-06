a06facce15e6d64d7642dd65b846227514a88819
ben <biovino@gmail.com>
2024-03-06 14:02:46 -0500

    multi cpu works

--- scripts/make_db.py
+++ scripts/make_db.py
@@ -6,15 +6,14 @@ __date__ = "12/18/23"
 
 import argparse
 import datetime
-import time
 import logging
 import os
 import torch
 import torch.multiprocessing as mp
-from embedding import Model, Embedding
+from embedding import Model, Batch
 from fingerprint import Fingerprint
 from database import Database
-from util import load_seqs
+from util import yield_seqs
 
 log_filename = 'data/logs/make_db.log'  #pylint: disable=C0103
 os.makedirs(os.path.dirname(log_filename), exist_ok=True)
@@ -22,37 +21,46 @@ logging.basicConfig(filename=log_filename, filemode='w',
                      level=logging.INFO, format='%(message)s')
 
 
-def get_fprint(seq: tuple, model: Model, device: str, args: argparse.Namespace):
-    """Returns a Fingerprint object for a given sequence.
+def queue_cpu(cpu_queue: mp.Queue, args: argparse.Namespace):
+    """Moves through queue of Fingerprints and quantizes their embeddings.
 
     Args:
-        seqs (tuple): Tuple of protein ID and sequence
-        model (Model): Object containing encoder and tokenzier
-        device (str): gpu/cpu
+        queue (mp.Queue): Queue of embeddings to fingerprint
         args (argparse.Namespace): Command line arguments
-
-    Returns:
-        Fingerprint: Fingerprint object containing DCT fingerprints
     """
 
-    # Embedding
-    pid, seq = seq[0], seq[1]
-    nowt = time.time()
-    emb = Embedding(pid=pid, seq=seq)
-    emb.embed_seq(model, device, args.layers, args.maxlen)
-
-    # Fingerprint
-    fprint = Fingerprint(pid=pid, seq=seq, embed=emb.embed, contacts=emb.contacts)
-    fprint.reccut(2.6)
-    fprint.quantize(args.quantdims)
-    endt = time.time()
-    logging.info('%s Sequence: %s, Length: %s, Time: %s',
-                  datetime.datetime.now(), pid, len(seq), endt-nowt)
-
-    return fprint
+    while True:
+        fp = cpu_queue.get()
+        if fp is None:
+            break
+        fp.reccut(2.6)
+        fp.quantize(args.quantdims)
+        logging.info(f'{datetime.datetime.now()} Fingerprinted {fp.pid}')
 
 
-def queue_seq(rank: int, queue: mp.Queue, args: argparse.Namespace):
+def fprint_cpu(batch: list, args: argparse.Namespace):
+    """Puts batches of Fingerprints in queue to be quantized on cpu(s).
+
+    Args:
+        batch (list): List of fingerprints to quantize
+        args (argparse.Namespace): Command line arguments
+    """
+
+    cpu_queue = mp.Queue()
+    processes = []
+    for _ in batch:
+        proc = mp.Process(target=queue_cpu, args=(cpu_queue, args))
+        proc.start()
+        processes.append(proc)
+    for fp in batch:
+        cpu_queue.put(fp)
+    for _ in range(len(batch)):  # send None to each process to signal end of queue
+        cpu_queue.put(None)
+    for proc in processes:
+        proc.join()
+
+
+def queue_gpu(rank: int, queue: mp.Queue, args: argparse.Namespace):
     """Moves through queue of sequences to fingerprint and add to database.
 
     :param rank: GPU to load model on
@@ -61,71 +69,101 @@ def queue_seq(rank: int, queue: mp.Queue, args: argparse.Namespace):
     """
 
     # Load tokenizer and encoder
-    device = torch.device(f'cuda:{rank}')  #pylint: disable=E1101
+    device = torch.device(f'cuda:{rank}')
     model = Model('esm2', 't30')  # pLM encoder and tokenizer
     model.to_device(device)
 
-    # Fingerprint each sequence and add to database
-    db = Database()
+    # Embed batches of sequences
+    cpu_queue = []
     while True:
-        seq = queue.get()
-        if seq is None:
+        seqs = queue.get()
+        if seqs is None:
             break
-        fprint = get_fprint(seq, model, device, args)
-        db.add_fprint(fprint)
+        batch = Batch(seqs, model, device)
+        batch.embed_batch(args.layers, args.maxlen)
 
-    db.save_db(args.dbfile)
+        # Create Fingerprint object and add to queue
+        for emb in batch.embeds:
+            fp = Fingerprint(pid=emb.pid, seq=emb.seq, embed=emb.embed, contacts=emb.contacts)
+            cpu_queue.append(fp)
+        if len(cpu_queue) >= args.cpu:
+            fprint_cpu(cpu_queue, args)
+            cpu_queue = []
 
 
-def embed_gpu(seqs: dict, args: argparse.Namespace):
-    """Embeds sequences on gpu(s).
+def embed_gpu(args: argparse.Namespace):
+    """Puts batches of sequences in queue to be embedded on gpu(s).
 
     Args:
-        seqs (dict): Dictionary of protein sequences
         args (argparse.Namespace): Command line arguments
     """
 
     mp_queue = mp.Queue()
     processes = []
-    for rank in args.gpulist:
-        proc = mp.Process(target=queue_seq, args=(args.gpulist[rank], mp_queue, args))
+    for rank in range(args.gpu):
+        proc = mp.Process(target=queue_gpu, args=(rank, mp_queue, args))
         proc.start()
         processes.append(proc)
-    for seq in seqs.items():  # seq is a tuple of protein ID and sequence
-        mp_queue.put(seq)
-    for _ in range(len(args.gpulist)):  # send None to each process to signal end of queue
+    for seqs in yield_seqs(args.fafile, args.maxlen):
+        mp_queue.put(seqs)
+    for _ in range(args.gpu):  # send None to each process to signal end of queue
         mp_queue.put(None)
     for proc in processes:
         proc.join()
 
 
+def embed_cpu(args: argparse.Namespace):
+    """Embeds sequences on cpu.
+    
+    Args:
+        args (argparse.Namespace): Command line arguments
+    """
+
+    model = Model('esm2', 't30')
+    device = torch.device('cpu')
+    model.to_device(device)
+    
+    # Embed one sequence at a time (batching on cpu is very slow)
+    cpu_queue = []
+    for seqs in yield_seqs(args.fafile, 1):
+        batch = Batch(seqs, model, device)
+        batch.embed_batch(args.layers, args.maxlen)
+
+        # Create Fingerprint object and add to queue
+        for emb in batch.embeds:
+            fp = Fingerprint(pid=emb.pid, seq=emb.seq, embed=emb.embed, contacts=emb.contacts)
+            cpu_queue.append(fp)
+        if len(cpu_queue) >= args.cpu:
+            fprint_cpu(cpu_queue, args)
+            cpu_queue = []
+
+
 def main():
-    """Main function
+    """Sequences from a fasta file of protein sequences go through two processes:
+
+    1. Embedding: sequences are embedded using ESM2, recommended to be done on GPU.
+    2. Fingerprinting: domains are cut and quantized, performed on CPU.
+
+    The resulting fingerprints are then written to a database file. Increasing the number of
+    both available GPUs and CPUs will speed up the process. Make sure --maxlen is set to a
+    value that is appropriate for the available memory.
     """
 
     parser = argparse.ArgumentParser()
     parser.add_argument('--fafile', type=str, required=True, help='fasta file to embed')
     parser.add_argument('--dbfile', type=str, required=True, help='db file to write to')
     parser.add_argument('--maxlen', type=int, default=1000, help='max sequence length to embed')
-    parser.add_argument('--gpu', type=bool, default=False, help='gpu (True) or cpu (False)')
-    parser.add_argument('--gpulist', type=int, nargs='+', default=[0], help='list of cuda devices')
+    parser.add_argument('--cpu', type=int, default=1, help='number of cpus to use')
+    parser.add_argument('--gpu', type=int, required=False, help='number of gpus to use')
     parser.add_argument('--layers', type=int, nargs='+', default=[15, 21], help='embedding layers')
     parser.add_argument('--quantdims', type=int, nargs='+', default=[3, 80, 3, 80],
                          help='quantization dimensions, each pair of dimensions quantizes a layer')
     args = parser.parse_args()
 
-    seqs = load_seqs(args.fafile)  # all sequences in memory
     if args.gpu:
-        embed_gpu(seqs, args)
+        embed_gpu(args)
     else:
-        model = Model('esm2', 't30')
-        device = torch.device('cpu')
-        model.to_device(device)
-        db = Database()
-        for seq in seqs.items():
-            fprint = get_fprint(seq, model, device, args)
-            db.add_fprint(fprint)
-        db.save_db(args.dbfile)
+        embed_cpu(args)
 
 
 if __name__ == '__main__':
