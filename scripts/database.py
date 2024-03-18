37b5efb1df103dbaca15b774ce1b963c011d6309
ben <biovino@gmail.com>
2024-03-18 15:28:40 -0400

    yield seqs from sqlite db

--- scripts/database.py
+++ scripts/database.py
@@ -17,18 +17,28 @@ class Database:
         cur (sqlite3.Cursor): Cursor for database.
     """
 
+
     def __init__(self, path: str):
-        """Initializes database.
+        """Initializes database file and cursor. Will create new database if fasta file is given.
         
         Args:
             path (str): Path to database file.
         """
 
-        # Check if fasta
         self.path = path
         if '.fa' in self.path:
             seqs = self.read_fasta()
             self.init_db(seqs)
+        else:
+            self.conn = sqlite3.connect(self.path)
+            self.cur = self.conn.cursor()
+
+    
+    def close(self):
+        """Closes the database connection.
+        """
+
+        self.conn.close()
 
 
     def read_fasta(self) -> dict:
@@ -42,10 +52,11 @@ class Database:
         with open(self.path, 'r', encoding='utf8') as f:
             for line in f:
                 if line.startswith('>'):
-                    pid = line.strip()
+                    pid = line.strip().split()[0]
                     seqs[pid] = ''
                 else:
                     seqs[pid] += line.strip()
+        seqs = {k: v for k, v in sorted(seqs.items(), key=lambda item: len(item[1]))}
         
         return seqs
     
@@ -57,13 +68,13 @@ class Database:
             seqs (dict): Dictionary of sequences.
         """
 
-        filename = os.path.splitext(self.path)[0]
-        self.conn = sqlite3.connect(f'{filename}.db')
+        self.path = os.path.splitext(self.path)[0]
+        self.conn = sqlite3.connect(f'{self.path}.db')
         self.cur = self.conn.cursor()
 
         # Create table
         table = """CREATE TABLE sequences (
-                id text PRIMARY KEY,
+                pid text PRIMARY KEY,
                 sequence text NOT NULL,
                 length integer NOT NULL,
                 domains text,
@@ -72,8 +83,43 @@ class Database:
         self.cur.execute(table)
 
         # Insert sequences
-        insert = """ INSERT INTO sequences(id, sequence, length, domains, fingerprint)
+        insert = """ INSERT INTO sequences(pid, sequence, length, domains, fingerprint)
             VALUES(?, ?, ?, ?, ?) """
         for pid, seq in seqs.items():
             self.cur.execute(insert, (pid, seq, len(seq), '', None))
         self.conn.commit()
+
+
+    def yield_seqs(self, maxlen: int, dim1: int = 3, dim2: int = 80):
+        """Yields sequences from the database.
+
+        Args:
+            maxlen (int): Maximum length of total sequence to yield
+            dim1 (int): First dimension of quantization.
+            dim2 (int): Second dimension of quantization.
+
+        Yields:
+            list: List of tuples where elements are protein ID and sequence.
+        """
+
+        seqs, curr_len, min_size = [], 0, dim1*dim2
+        select = """ SELECT pid, sequence, length FROM sequences WHERE domains == ''"""
+        for row in self.cur.execute(select):
+            pid, seq, length = row
+            if (length-2) * dim2 < min_size:  # Short sequences can't be quantized
+                continue
+            curr_len += length
+
+            # If list is too large, yield all but last and reset
+            if len(seqs) > 1 and curr_len > maxlen:
+                last_seq = seqs.pop()
+                yield seqs
+                curr_len = len(last_seq[1])
+                seqs = [(last_seq[0], last_seq[1])]
+            seqs.append((pid, seq))  # New sequence
+
+        # Last batch in file may be too large
+        if len(seqs) > 1 and curr_len > maxlen:
+            last_seq = seqs.pop()
+            yield seqs
+        yield [(last_seq[0], last_seq[1])]
