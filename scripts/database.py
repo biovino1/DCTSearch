ada7815e1ca3bb0aea2c6315ab19649f29c1286c
ben <biovino@gmail.com>
2024-03-18 13:50:08 -0400

    move db class from np to sqlite

--- scripts/database.py
+++ scripts/database.py
@@ -1,81 +1,79 @@
-"""Defines the Database class, which is used to store more than one Fingerprint object.
+"""Defines the Database class, which is used to manage the SQLite database.
 
 __author__ = "Ben Iovino"
-__date__ = "2/19/24"
+__date__ = "3/18/24"
 """
 
-from dataclasses import dataclass, field
-import numpy as np
+import sqlite3
+import os
 
 
-@dataclass
 class Database:
-    """This class stores multiple Fingerprint objects.
+    """Initializes .db file and manages database operations.
 
     Attributes:
-        pids (list): Protein IDs.
-        idx (list): List of indices for domain/fingerprint list positions.
-        doms (list): Domain boundaries.
-        quants (list): Quantizations.
+        path (str): Path to database file.
+        conn (sqlite3.Connection): Connection to database.
+        cur (sqlite3.Cursor): Cursor for database.
     """
-    file: str = field(default_factory=str)
-    pids: list = field(default_factory=list)
-    idx: list = field(default_factory=list)
-    doms: list = field(default_factory=list)
-    quants: list = field(default_factory=list)
 
-
-    def __post_init__(self):
-        """If file is given, load database from file.
+    def __init__(self, path: str):
+        """Initializes database.
+        
+        Args:
+            path (str): Path to database file.
         """
 
-        if self.file:
-            self.load_db(self.file)
+        # Check if fasta
+        self.path = path
+        if '.fa' in self.path:
+            seqs = self.read_fasta()
+            self.init_db(seqs)
 
 
-    def load_db(self, dbfile: str):
-        """Loads a database from a npz file as four arrays: protein ids, domain/fingerprint
-        indices, domain boundaries, and quantizations.
+    def read_fasta(self) -> dict:
+        """Returns a dictionary of sequences from a fasta file.
+
+        Returns:
+            dict: Dictionary of sequences.
+        """
+
+        seqs = {}
+        with open(self.path, 'r', encoding='utf8') as f:
+            for line in f:
+                if line.startswith('>'):
+                    pid = line.strip()
+                    seqs[pid] = ''
+                else:
+                    seqs[pid] += line.strip()
+        
+        return seqs
+    
+
+    def init_db(self, seqs: dict):
+        """Creates a new database file and fills table with sequences.
 
         Args:
-            dbfile (str): Path to input file.
+            seqs (dict): Dictionary of sequences.
         """
 
-        db = np.load(dbfile)
-        self.pids = db['pids']
-        self.idx = db['idx']
-        self.doms = db['doms']
-        self.quants = db['quants']
-        db.close()
+        filename = os.path.splitext(self.path)[0]
+        self.conn = sqlite3.connect(f'{filename}.db')
+        self.cur = self.conn.cursor()
 
+        # Create table
+        table = """CREATE TABLE sequences (
+                id text PRIMARY KEY,
+                sequence text NOT NULL,
+                length integer NOT NULL,
+                domains text,
+                fingerprint blob
+                ); """
+        self.cur.execute(table)
 
-    def add_fprint(self, fprint):
-        """Adds a Fingerprint object to the database.
-
-        Args:
-            fprint (Fingerprint): Fingerprint object.
-        """
-
-        # Get next starting index for domain boundaries
-        if self.idx:
-            prev_doms = len(self.doms[self.idx[-1]:])
-            self.idx.append(self.idx[-1] + prev_doms)
-        else:
-            self.idx.append(0)
-
-        # Add protein ID, domain boundaries, and quantizations
-        self.pids.append(fprint.pid)
-        self.doms.extend(fprint.domains)
-        self.quants.extend(fprint.quants.values())
-
-
-    def save_db(self, dbfile: str):
-        """Saves the database to a npz file as four arrays: protein ids, domain/fingerprint
-        indices, domain boundaries, and quantizations.
-
-        Args:
-            dbfile (str): Path to output file.
-        """
-
-        np.savez_compressed(dbfile, pids=self.pids, idx=self.idx,
-                             doms=self.doms, quants=self.quants)
+        # Insert sequences
+        insert = """ INSERT INTO sequences(id, sequence, length, domains, fingerprint)
+            VALUES(?, ?, ?, ?, ?) """
+        for pid, seq in seqs.items():
+            self.cur.execute(insert, (pid, seq, len(seq), '', None))
+        self.conn.commit()
