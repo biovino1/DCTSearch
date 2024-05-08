2ff7daf5ef145977bc9a08d1ab9214215bd9df85
ben <biovino@gmail.com>
2024-05-08 09:46:06 -0400

    better result reading

--- bench/cath/run_dct.py
+++ bench/cath/run_dct.py
@@ -29,15 +29,15 @@ def get_queries(file: str) -> dict[str, str]:
     with open(file, 'r', encoding='utf8') as file:
         for line in file:
             if line.startswith('>'):
-                line = line.split()
-                queries[line[0][1:]] = line[1]  # key: PID, value: classification
+                line = line.split('|')
+                queries[line[0][1:]] = line[1].strip()  # key: PID, value: classification
 
     return queries
 
 
 def search_cath20(path: str, queries: dict[str, str], khits: int):
     """Similar to search_db in query_db.py, but queries CATH20 db with sequences from
-    cath20_queries.txt.
+    cath20_queries.txt (which already exist in the db).
 
     Args:
         path (str): Path to database and index files
@@ -47,7 +47,7 @@ def search_cath20(path: str, queries: dict[str, str], khits: int):
     """
 
     db = Database(f'{path}/cath20.db')
-    index = faiss.read_index(f'{path}/cath20.db'.replace('.db', '.index'))
+    index = faiss.read_index(f'{path}/cath20.index')
     for pid, fam in queries.items():
         qfps = db.load_fprints(pid=f'{pid}|{fam}')
         que_arrs = np.array([fp[1] for fp in qfps])
@@ -59,36 +59,44 @@ def search_cath20(path: str, queries: dict[str, str], khits: int):
 
 
 def dct_results(path: str) -> dict[str, set]:
-    """Returns dictionary of query PID's and a set of TP's up to first FP.
+    """Returns a dictionary of query PID's and their top hits until the first FP. Only one hit per
+    unique sequence is counted, hence the use of a set. This allows for the calculation of both
+    AUC1 and top1 scores.
 
     Args:
         path (str): Path to results file
 
     Returns:
-        dict[str, set]: key: query PID, value: set of TP sequences
+        dict[str, set]: key: query PID, value: set of top hits until the first FP
     """
 
     with open(f'{path}/results_dct.txt', 'r', encoding='utf8') as file:
-        results: dict[str, set] = {}
-        query, fp = '', False 
+        results, curr_query = {}, ''
         for line in file:
+            if line == '\n':
+                continue
             line = line.split()
-            if line == []:  # Skip empty lines
-                continue
-            if line[1] != query:  # New query
-                query = line[1]
-                fp = False
-            result = line[5]
 
-            # Ignore if already reached a false positive or self hit
-            if (fp or query == result):
+            # If query had FP, continue until new query
+            query = line[1]
+            result = line[5]
+            if query == curr_query:
                 continue
-            
-            # Check homologous superfamily classification
-            if query.split('|')[1] == result.split('|')[1]:  # True positive
-                results[query] = results.get(query, set()) | set([result])
-            else:  # False positive
-                fp = True
+            results[query] = results.get(query, set())
+
+            # Ignore self-hits
+            if query == result:
+                continue
+
+            # Stop counting hits for current query if domains are different
+            query_dom = query.split('|')[1]
+            result_dom = result.split('|')[1]
+            if query_dom != result_dom:
+                curr_query = query
+                continue
+
+            # Add hit to results
+            results[query].add(result)
 
     return results
 
