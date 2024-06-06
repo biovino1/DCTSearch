4f6514f38fae909d4f9a8bb0c9f57aa33f30107c
ben <biovino@gmail.com>
2024-06-06 09:28:25 -0400

    works for multi-dom prots

--- bench/plot_results.py
+++ bench/plot_results.py
@@ -8,25 +8,32 @@ import argparse
 import matplotlib.pyplot as plt
 
 
-def get_fams(file: str) -> dict[str, list]:
+def get_classes(file: str, claisf: str) -> dict[str, list]:
     """Returns a dictionary of family names and the sequences that belong to them.
 
     Args:
         file (str): Path to queries file
+        clasif (str): Desired classification ID of seqs (SCOP ONLY)
+            fam - Family ID
+            supfam - Superfamily ID
+            fold - Fold ID
 
     Returns:
-        dict[str, list]: key: family name, value: list of sequences
+        dict[str, list]: key: classification ID, value: list of sequences
     """
 
     with open(file, 'r', encoding='utf8') as file:
-        fams: dict[str, list] = {}
+        cids: dict[str, list] = {}  # key: classification ID, value: list of sequences
         for line in file:
             if line.startswith('>'):
                 line = line.split('|')
-                dom, fam = line[0], line[1].strip()
-                fams[fam] = fams.get(fam, []) + [dom]
+                seq = line[0]
+                for cid in line[1].strip().split(';'):  # For multi-domain seqs (i.e. scop benchmark)
+                    if cid not in cids:
+                        cids[cid] = set()
+                    cids[cid].add(seq)
 
-    return fams
+    return cids
 
 
 def read_results(path: str, query_ind: int, result_ind: int) -> dict[str, set]:
@@ -62,9 +69,9 @@ def read_results(path: str, query_ind: int, result_ind: int) -> dict[str, set]:
                 continue
 
             # Stop counting hits for current query if domains are different
-            query_dom = query.split('|')[1]
-            result_dom = result.split('|')[1]
-            if query_dom != result_dom:
+            query_dom = query.split('|')[1].split(';')
+            result_dom = result.split('|')[1].split(';')
+            if not any([dom in query_dom for dom in result_dom]):
                 curr_query = query
                 continue
 
@@ -74,52 +81,55 @@ def read_results(path: str, query_ind: int, result_ind: int) -> dict[str, set]:
     return results
 
 
-def eval_scores(fams: dict[str, list], results: dict[str, set], method: str):
+def eval_scores(cids: dict[str, list], results: dict[str, set], method: str):
     """Returns dict of AUC1 scores for each query. AUC1 is calculated as the number of TP's
     up to the 1st FP divided by the number of sequences in the family.
 
     Args:
-        path (str): Path to results file
+        cids (dict[str, list]): Dictionary of classification ID's and a list of sequences.
         results (dict[str, set]): Dictionary of query PID's and a set of TP's up to the 1st FP.
-        method (str): Method being evaluated
+        method (str): Method being evaluated.
 
     Returns:
         dict[str, float]: key: query PID, value: AUC1 score
     """
-    
+
     # Calculate scores
     auc_scores: dict[str, float] = {}
-    fam_scores: dict[str, list[float, float]] = {}  # keep track of tp/total for each family
+    cid_scores: dict[str, list[float, float]] = {}  # keep track of tp/total for each class
     top1, total = 0, 0
     for query, tps in results.items():
 
         # Get family and update scores
-        fam = query.split('|')[1]  # i.e. 16vpA00|3.30.930.10
-        fam_scores[fam] = fam_scores.get(fam, [0, 0])
+        qcids = query.split('|')[1]  # i.e. 16vpA00|3.30.930.10
+        cid_scores[qcids] = cid_scores.get(qcids, [0, 0])
 
         # top1 - Add 1 if top hit belongs to same family
         # fam_scores - Add 1 to total and top1 if top hit belongs to same family
         if len(tps) > 0:  # If there are hits, that means top hit is TP
-            fam_scores[fam][0] += 1
+            cid_scores[qcids][0] += 1
             top1 += 1
-        fam_scores[fam][1] += 1
+        cid_scores[qcids][1] += 1
         total += 1
 
         # Calculate AUC1 score
         try:
-            score = len(tps) / (len(fams[fam])-1)  # ignore self hit
+            possible_hits = 0
+            for cid in qcids.split(';'):
+                possible_hits += len(cids[cid])
+            score = len(tps) / (possible_hits-1)  # ignore self hit
         except ZeroDivisionError:
             score = 0
         auc_scores[query] = auc_scores.get(query, 0) + score  # ignore self hit
 
     # For each family, find number of true positives over total family size
     fam_total = 0
-    for fam, (tp, tot) in fam_scores.items():
+    for fam, (tp, tot) in cid_scores.items():
         fam_total += (1/(tot)) * tp
 
     # Print results
     print(f'{method}:')
-    print(f'QNormTop1: {(fam_total):.2f}/{len(fam_scores)}, {fam_total/len(fam_scores)*100:.2f}%')
+    print(f'QNormTop1: {(fam_total):.2f}/{len(cid_scores)}, {fam_total/len(cid_scores)*100:.2f}%')
     print(f'QRawTop1: {top1}/{total}, {(top1/total * 100):.2f}%\n')
     
     return auc_scores
@@ -155,18 +165,19 @@ def main():
 
     parser = argparse.ArgumentParser()
     parser.add_argument('--bench', type=str, required=True, help='benchmark to query')
+    parser.add_argument('--cid', type=str, default='fam', help='classification of sequences')
     args = parser.parse_args()
 
     # Determine query and db files
     if args.bench == 'cath':
         path = 'bench/cath/data'
-        fams = get_fams(f'{path}/cath20_queries.fa')
+        cids = get_classes(f'{path}/cath20_queries.fa', args.cid)
     elif args.bench == 'pfam':
         path = 'bench/pfam/data'
-        fams = get_fams(f'{path}/pfam20.fa')
+        cids = get_classes(f'{path}/pfam20.fa', args.cid)
     elif args.bench == 'scop':
         path = 'bench/scop/data'
-        fams = get_fams(f'{path}/query.fa')
+        cids = get_classes(f'{path}/target.fa', args.cid)
 
     # Read results after running DCTSearch and MMseqs2
     dct_res = read_results(f'{path}/results_dct.txt', 1, 5)
@@ -175,9 +186,9 @@ def main():
 
     # Plot AUC1 scores for each query
     scores = []
-    scores.append(eval_scores(fams, dct_res, 'DCTSearch'))
-    scores.append(eval_scores(fams, mean_res, 'ProtT5-Mean'))
-    scores.append(eval_scores(fams, mmseqs_res, 'MMseqs2'))
+    scores.append(eval_scores(cids, dct_res, 'DCTSearch'))
+    scores.append(eval_scores(cids, mean_res, 'ProtT5-Mean'))
+    scores.append(eval_scores(cids, mmseqs_res, 'MMseqs2'))
     methods = ['DCTSearch', 'ProtT5-Mean', 'MMseqs2']
     graph_results(scores, args.bench, methods)
 
