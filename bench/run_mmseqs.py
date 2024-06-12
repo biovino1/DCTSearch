f7ea451d2cd6f2f6b5e8e71de8965d705f1823ef
ben <biovino@gmail.com>
2024-06-12 13:49:54 -0400

    mmseqs-sens

--- bench/run_mmseqs.py
+++ bench/run_mmseqs.py
@@ -12,10 +12,7 @@ sys.path.append(os.getcwd()+'/src')  # Add src to path
 
 
 def main():
-    """
-    Args:
-        --bench (str): benchmark to query
-            Can be 'cath', 'pfam', or 'scop'
+    """MMseqs ran like in their benchmark.
     """
 
     parser = argparse.ArgumentParser()
@@ -25,19 +22,29 @@ def main():
     # Determine query and db files
     if args.bench == 'cath':
         path = 'bench/cath/data'
-        query = 'cath20_queries.fa'
-        db = 'cath20.fa'
+        query = 'cath20_queries'
+        db = 'cath20'
     elif args.bench == 'pfam':
         path = 'bench/pfam/data'
-        query = 'pfam20.fa'
-        db = 'pfam20.fa'
+        query = 'pfam20'
+        db = 'pfam20'
     elif args.bench == 'scop':
         path = 'bench/scop/data'
-        query = 'query.fa'
-        db = 'target.fa'
+        query = 'query'
+        db = 'target'
 
-    sp.run(['mmseqs', 'easy-search', f'{path}/{query}', f'{path}/{db}',
-            f'{path}/results_mmseqs.txt', f'{path}/tmp', '-e', '10000', '-s', '7.5'])
+    # Create MMseqs2 databases
+    sp.run(['mmseqs', 'createdb', f'{path}/{query}.fa', f'{path}/mmseqs/{query}DB'])
+    sp.run(['mmseqs', 'createdb', f'{path}/{db}.fa', f'{path}/mmseqs/{db}DB'])
+    sp.run(['mmseqs', 'createindex', f'{path}/mmseqs/{db}DB', '-k', '7', '--split', '1'])
+
+    # Search and convert results
+    os.makedirs(f'{path}/mmseqs', exist_ok=True)
+    sp.run(['mmseqs', 'search', f'{path}/mmseqs/{query}DB', f'{path}/mmseqs/{db}DB',
+            f'{path}/mmseqs/results_sense_aln', f'{path}/tmp', '--min-ungapped-score', '0',
+            '-e', '10000', '-k', '7', '--k-score', '80', '--max-seqs', '4000', '-s', '7.5'])
+    sp.run(['mmseqs', 'convertalis', f'{path}/mmseqs/{query}DB', f'{path}/mmseqs/{db}DB',
+            f'{path}/mmseqs/results_sense_aln', f'{path}/results_mmseqs.txt'])
 
 
 if __name__ == '__main__':
